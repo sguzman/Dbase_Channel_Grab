a9ff4a76f5a9435440a9714a091635b1c8da6958
Salvador Guzman <guzmansalv@gmail.com>
2018-09-18 13:50:57 -0700

    insert channels into table

--- src/insert_channel.py
+++ src/insert_channel.py
@@ -3,58 +3,73 @@ import requests
 import bs4
 import psycopg2
 
-fd = open('../channels.txt', 'r')
 
-yt_base = 'https://www.youtube.com/channel/'
+def insert(conn, data):
+    sql_insert_chann = 'INSERT INTO youtube.channels.channel (joined, channel_id, channel_title) VALUES (%s, %s, %s)'
+    cursor = conn.cursor()
+    cursor.execute(sql_insert_chann, data)
+    conn.commit()
 
-sql_insert_chann = 'INSERT INTO youtube.channels.channel (joined, channel_id, channel_title) VALUES (%s, %s, %s)'
 
-connection = psycopg2.connect(user='root', password='', host='127.0.0.1', port='5432', database='youtube')
+def title(soup):
+    return soup.find('meta', property='og:title')['content']
+
+
+def joined(raw_joined):
+    return datetime.datetime.strptime(raw_joined[raw_joined.index(' ') + 1:], '%b %d, %Y')
+
+
+def about_soup(chan_id):
+    url = 'https://www.youtube.com/channel/%s/about' % chan_id
+    req = requests.get(url)
+    html_body = req.text
+
+    return bs4.BeautifulSoup(html_body, 'html.parser')
 
 
 def process(chan_id):
-    url = yt_base + chan_id + '/about'
-    req = requests.get(url)
-    html_body = req.text
-    soup = bs4.BeautifulSoup(html_body, 'html.parser')
-
+    soup = about_soup(chan_id)
     a = []
     for i in soup.findAll('span', class_='about-stat'):
-        a.append(i.text)
+        if i.text.startswith('Joined'):
+            a.append(joined(i.text))
 
-    a[0] = chan_id
+    a.append(chan_id)
+    a.append(title(soup))
 
-    if len(a) is 2:
-        a[1] = datetime.datetime.strptime(a[1][a[1].index(' ') + 1:], '%b %d, %Y')
-
-    if len(a) is 3:
-        a[2] = datetime.datetime.strptime(a[2][a[2].index(' ') + 1:], '%b %d, %Y')
-        a = [a[0], a[2]]
-
-    a.append(soup.find('meta', property='og:title')['content'])
-    a.append(soup.find('meta', property='og:description')['content'])
-
-    return a[1], a[0], a[2]
+    return a
 
 
-def main():
-    PostgreSQL_select_Query = "select channel_id from youtube.channels.channel"
-    cursor = connection.cursor()
-    cursor.execute(PostgreSQL_select_Query)
+def select(conn):
+    postgresql_select_query = "select channel_id from youtube.channels.channel"
+    cursor = conn.cursor()
+    cursor.execute(postgresql_select_query)
     records = cursor.fetchall()
 
     ignore = set()
     for i in records:
         ignore.add(i[0])
 
-    channels = [(x[:-1] if x[-1] == '\n' else x) for x in fd.readlines()]
-    for i in channels:
-        if i not in ignore:
+    return ignore
+
+
+def filter_chans(conn):
+    ignore = select(conn)
+    readlines_set = set([(x[:-1] if x[-1] == '\n' else x) for x in open('../channels.txt', 'r').readlines()])
+
+    return readlines_set.difference(ignore)
+
+
+def main():
+    connection = psycopg2.connect(user='root', password='', host='127.0.0.1', port='5432', database='youtube')
+    clean_chans = filter_chans(connection)
+    for i in clean_chans:
+        try:
             data = process(i)
             print(data)
-            cursor = connection.cursor()
-            cursor.execute(sql_insert_chann, data)
-            connection.commit()
+            insert(connection, data)
+        except Exception as e:
+            print(str(e))
 
 
 main()
