68316a0234a8d4606113a9184bafbb072b139c61
Salvador Guzman <guzmansalv@gmail.com>
2018-09-21 19:21:50 -0700

    do not save api key lol

--- src/insert_videos.py
+++ src/insert_videos.py
@@ -1,7 +1,7 @@
-import datetime
 import requests
-import bs4
 import psycopg2
+import json
+import os
 
 
 def insert_vid(conn, data):
@@ -12,8 +12,61 @@ def insert_vid(conn, data):
     cursor.close()
 
 
-def process(i):
-    
+def get_vid_from_channel(channel_id, api_key):
+    url = 'https://www.googleapis.com/youtube/v3/search'
+    param1 = {
+        'part': 'id',
+        'channelId': channel_id,
+        'type': 'video',
+        'key': api_key
+    }
+    contents = requests.get(url, params=param1).text
+    return json.loads(contents)
+
+
+def get_vid_from_channel_next_page(channel_id, api_key, next_page):
+    url3 = 'https://www.googleapis.com/youtube/v3/search'
+    param3 = {
+        'part': 'id',
+        'nextPage': next_page,
+        'channelId': channel_id,
+        'type': 'video',
+        'key': api_key
+    }
+    contents = requests.get(url3, params=param3).text
+    return json.loads(contents)
+
+
+def get_vid(video_id, api_key):
+    url2 = 'https://www.googleapis.com/youtube/v3/videos'
+    param2 = {
+        'part': 'contentDetails,id,liveStreamingDetails,localizations,recordingDetails,snippet,status,topicDetails',
+        'id': video_id,
+        'key': api_key
+    }
+
+    contents2 = requests.get(url2, params=param2).text
+    return json.loads(contents2)
+
+
+def process(channel_id, api_key):
+    a = []
+    json_data = get_vid_from_channel(channel_id, api_key)
+
+    next_page = json_data['nextPageToken']
+    items = json_data['items']
+    for page in range(json_data['pageInfo']['totalResults']):
+        print(page)
+        for item in items:
+            json_data2 = get_vid(item['id']['videoId'], api_key)
+            print(item['id']['videoId'])
+            a.append(json_data2)
+
+        json_data = get_vid_from_channel_next_page(channel_id, api_key, next_page)
+        next_page = json_data['nextPageToken']
+        items = json_data['items']
+
+    return a
 
 
 def select_chan(conn):
@@ -35,11 +88,12 @@ def select_chan(conn):
 def main():
     connection = psycopg2.connect(user='root', password='', host='127.0.0.1', port='5432', database='youtube')
     table_chans = set(select_chan(connection))
+    api_key = os.environ['API_KEY']
 
-    for i in table_chans:
+    for chan_id in table_chans:
         try:
-            data = process(i)
-            print(data)
+            print(chan_id[0])
+            data = process(chan_id, api_key)
         except Exception as e:
             print(e)
             exit(1)
